0a9dd7a546e0db7d34b2c35c97f2cf624fc2ce4a
adkt17 <76532427+adkt17@users.noreply.github.com>
2023-10-12 11:32:32 +0530

    Update streamlit_app.py
    Added one more item.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale Spinach and Rocket Smoothie')
-streamlit.text('Hard Boiled free Range Egg')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale Spinach and Rocket Smoothie')
+streamlit.text('🐔 🥑🍞Hard Boiled free Range Egg')
+streamlit.text('🥑🍞Avocadp Toast)
