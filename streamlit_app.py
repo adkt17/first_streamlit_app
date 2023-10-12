98218b29fc1fe867030de06d9d6fb4423a3d6b3b
adkt17 <76532427+adkt17@users.noreply.github.com>
2023-10-12 11:34:06 +0530

    Update streamlit_app.py
    Adding Customisation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale Spinach and Rocket Smoothie')
 streamlit.text('🐔 🥑🍞Hard Boiled free Range Egg')
 streamlit.text('🥑🍞Avocadp Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
